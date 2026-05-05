85edda4ac664e9a739a66f0676308fb3c8bde2e4
Vikram Choudhary <vikramchoudhary3107@gmail.com>
2026-05-05 10:14:09 +0530

    Implemented more hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
+import ProductViewer from './Components/ProductViewer'
+import gsap from 'gsap'
+import { ScrollTrigger, SplitText } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <Hero />
+      <ProductViewer />
     </main>
   )
 }
